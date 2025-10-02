a5fcc70fe466d95084230790c09f02f42d63ae3d
afzalvfx <afzalcreatives@gmail.com>
2025-10-02 12:29:00 +0530

    file updated

--- 09_todo_basics/main.js
+++ 09_todo_basics/main.js
@@ -6,29 +6,51 @@
 let tasks = []
 let resultelement = document.getElementById("result")
 
-function rendertask(){
+function rendertask(array){
     // reset result element
     resultelement.innerHTML = ""
     // append all values in result
-    tasks.forEach((item, index) => {
+    array.forEach((item, index) => {
         resultelement.innerHTML += `<div>
-        <h1>${item}</h1>
-        <button onclick="deletetask(${index})">Delete</button>
+        <h1><input type="checkbox" onchange="toggleComplete(${item.id})" ${item.isCompleted ? "checked" : ""}>${item.name} - ${item.isCompleted ? "Completed" : "" }</h1>
+        <button onclick="deletetask(${item.id})">Delete</button>
         </div>`
     })
 }
 
-function deletetask(index){
-    tasks.splice(index, 1)
-    rendertask()
+function deletetask(taskID){
+    tasks = tasks.filter(item => item.id != taskID)
+    rendertask(tasks)
 }
 
 document.getElementById("addbtn").addEventListener("click", ()=>{
     // get input value
     let value = document.getElementById("taskinput").value
+    let obj = {id: Date.now(), name: value, isCompleted: false}
 
     // push value to array
-    tasks.push(value)
-    rendertask()
+    tasks.push(obj)
+    rendertask(tasks)
     })
 
+document.getElementById("searchInput").addEventListener("keyup",  ()=>{
+    let search_key = document.getElementById("searchInput").value
+    let searchedTasks = tasks.filter(item =>{
+        return item.name.includes(search_key)
+    })
+    resultelement.innerHTML = ""
+    rendertask(searchedTasks)
+})
+
+function toggleComplete(taskID){
+    tasks = tasks.map((item) =>{
+        if (item.id==taskID) {
+            item.isCompleted = !item.isCompleted
+            return item
+        } else {
+            return item
+            
+        }
+    })
+    rendertask(tasks)
+}
